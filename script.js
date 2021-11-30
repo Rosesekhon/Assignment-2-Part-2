f2154345323af1349c3cabd032a369c0909fc1e9
Rose <rsekhon32@my.bcit.ca>
2021-11-30 12:24:31 -0800

    added files to new folder

---
+++ script.js
@@ -0,0 +1,97 @@
+
+/* INDEX PAGE #1*/
+function cleanUpIndex(){
+    const element = document.querySelectorAll(".contact");
+    for (let contact of element){
+        contact.remove()
+    }
+}
+  
+/* INDEX #2*/
+function createSingleIndex(single_obj){
+    return`
+        <a href="index.html">
+            <div class="contact">
+                <p> ${single_obj.name}</p>
+            </div>
+        </a>
+
+    `
+} 
+
+/* INDEX #3 */ 
+function renderIndex(array_contact){
+    const main = document.querySelector(".main"); 
+    for (element in array_contact){
+        main.insertAdjacentHTML('beforeend', createSingleIndex(element))
+    
+    };
+    // array_contact.forEach(element => {
+        
+    // });
+}
+
+const contactList = [
+    {
+        name : "Oliver Queen",
+        phone : "778-555-1234",
+        address : "101 Main St, Star City, USA",
+        email: "greenarrow@watchtower.com",
+    },
+    {
+        name :"Jessica Cruz",
+        phone : "123-456-5555",
+        address : "Portland Oregon",
+        email : "greenlantern@watchtower.com",
+    }
+]
+
+/* VEIW PAGE #1*/
+
+function cleanUpView(){
+    const element = document.querySelectorAll(".contactinfo")
+    for (let contactinfo of element){
+        contactinfo.remove()
+    };
+}
+
+/* VEIW #2 */
+
+function renderView(single_contact){
+  return`
+        <a href = "page3.html">
+            <div class ="contactinfo">
+                <p> ${single_contact.name}</p>
+            </div>
+        </a>
+
+  `
+
+}
+
+
+/* CREATE PAGE #1*/
+
+function cleanUpCreate(){
+    const element = document.querySelectorAll(".contactedit")
+    for (let contactedit of element){
+        contactedit.remove()
+    };
+}
+
+
+/*  CREATE #2 */
+
+function renderCreate(single_edit){
+    return`
+        <a href ="page2.html">
+            <div class = "contactedit">
+                <p> ${single_edit.name}</p>
+            </div>
+        </a>
+    `
+    
+}
+
+
+
